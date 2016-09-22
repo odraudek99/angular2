89a40fe31e4e2a3e77d945298a65e9430211c801
odraudek99 <odraudek99@gmail.com>
2016-09-21 23:38:06 -0500

    final course

--- curso/peliculas/app/app.component.ts
+++ curso/peliculas/app/app.component.ts
@@ -1,29 +1,63 @@
 import {Component} from "angular2/core";
-import {Pelicula} from "./model/pelicula";
+
+import {PeliculasListComponent} 
+from "./components/peliculas.list.component";
+
+import {PeliculasFooterComponent} 
+from "./components/peliculas-footer.component";
+
+import {ContactoComponent} 
+from "./components/contacto.component";
+
+import {CrearPeliculaComponent} 
+from "./components/crear-pelicula.component";
+
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
+
+
+
 
 @Component({
 	selector: "mi-app",
 
-	templateUrl: "app/view/peliculas.html"
+	templateUrl: "app/view/peliculas.html",
+	directives: [PeliculasListComponent, 
+				PeliculasFooterComponent, 
+				ContactoComponent,
+				CrearPeliculaComponent,
+				ROUTER_DIRECTIVES]
 })
+
+@RouteConfig([
+
+	{
+		path: "/peliculas", 
+		name: "Peliculas",
+		component: PeliculasListComponent,
+		useAsDefault: false
+	},{
+		path: "/contacto", 
+		name: "Contacto",
+		component: ContactoComponent,
+		useAsDefault: false
+	},{
+		path: "/crear-pelicula", 
+		name: "CrearPelicula",
+		component: CrearPeliculaComponent,
+		useAsDefault: false
+	},{
+		path: "/crear-pelicula/:titulo/:director/:anio", 
+		name: "CrearPeliculaBasadaEnOtra",
+		component: CrearPeliculaComponent,
+		useAsDefault: false
+	}
+
+	
+])
 
 export class AppComponent  {
 	
 	public titulo:string = "Hola Peliculas Angular2";
 	
-	public pelicula:Pelicula;
-
-	constructor() {
-		this.pelicula = new Pelicula(1,"Sherk","Lalo",2016);
-		
-
-		this.debug();
-	}
-
-
-
-	debug() {
-		console.log(this.pelicula);
-	}
 
 }
